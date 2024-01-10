cb9a2b6a804b26b41ebb26aa9f6cabd79e7161d3
rishiekumaraarachchi <rishieruween199611@gmail.com>
2024-01-10 13:48:30 +0530

    Initial commit

---
+++ src/main/java/com/example/sbrdemo/model/Student.java
@@ -0,0 +1,30 @@
+package com.example.sbrdemo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.NaturalId;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+    @NaturalId(mutable = true)
+    private String email;
+    private String department;
+
+
+}
