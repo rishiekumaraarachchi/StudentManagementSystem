3c4129d527aff6159ff6798a6bdf6f5ac608cbdc
rishiekumaraarachchi <rishieruween199611@gmail.com>
2024-01-10 21:28:11 +0530

    Student class

--- src/main/java/com/example/sbrdemo/model/Student.java
+++ src/main/java/com/example/sbrdemo/model/Student.java
@@ -1,10 +1,9 @@
 package com.example.sbrdemo.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
-import javax.persistence.GenerationType;
-import javax.persistence.GeneratedValue;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,12 +18,10 @@ import org.hibernate.annotations.NaturalId;
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String firstName;
     private String lastName;
     @NaturalId(mutable = true)
     private String email;
     private String department;
-
-
 }
---
+++ src/main/java/com/example/sbrdemo/service/IStudentService.java
@@ -0,0 +1,13 @@
+package com.example.sbrdemo.service;
+
+import com.example.sbrdemo.model.Student;
+
+import java.util.List;
+
+public interface IStudentService {
+    Student addStudent(Student student);
+    List<Student> getStudents();
+    Student updateStudent(Student student, Long id);
+    Student getStudentById(Long id);
+    void deleteStudent(Long id);
+}
